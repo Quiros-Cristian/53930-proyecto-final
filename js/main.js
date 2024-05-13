d74ea88fa2cb1753a0f1d5d21784394827d9b5a0
unknown <quiroscristian12@gmail.com>
2024-05-13 23:02:58 +0200

    agrego todo lo que faltaba

--- js/main.js
+++ js/main.js
@@ -8,7 +8,8 @@ let boton_oscuro = document.querySelector('.modo_oscuro')
 
 let vaciarCarrito = document.querySelector('.botonEliminar')
 
-let vaCarritoDos = document.querySelector('.remove')
+let eliminarProducto = document.querySelector('.remove')
+
 
 
 let carritoVacio = 0;
@@ -17,36 +18,36 @@ let carritoVacio = 0;
 // ~~~~~~~~~~~ Eventos para filtrar ~~~~~~~~~~~~
 
 
-document.addEventListener("keyup", e =>{
+document.addEventListener("keyup", e => {
 
-    if(e.target.matches("#buscador")){
+    if (e.target.matches("#buscador")) {
 
-        document.querySelectorAll(".divCard").forEach(buscar =>{
+        document.querySelectorAll(".divCard").forEach(buscar => {
 
             buscar.textContent.toLowerCase().includes(e.target.value.toLowerCase())
-                ?buscar.classList.remove("filtro")
-                :buscar.classList.add("filtro")
+                ? buscar.classList.remove("filtro")
+                : buscar.classList.add("filtro")
         })
 
     }
-    
+
 
 })
 
 
-document.addEventListener("click", e =>{
+document.addEventListener("click", e => {
 
-    if(e.target.matches(".filtrar")){
+    if (e.target.matches(".filtrar")) {
 
-        document.querySelectorAll(".divCard").forEach(buscar =>{
+        document.querySelectorAll(".divCard").forEach(buscar => {
 
             buscar.textContent.toLowerCase().includes(e.target.value.toLowerCase())
-                ?buscar.classList.remove("filtro")
-                :buscar.classList.add("filtro")
+                ? buscar.classList.remove("filtro")
+                : buscar.classList.add("filtro")
         })
 
     }
-    
+
 
 })
 
@@ -68,38 +69,65 @@ function render_ropa(data) {
     data.forEach((productos) => {
 
         let clon = document.querySelector('.clon_uno').content.cloneNode(true)
- 
+
         clon.querySelector('h5').innerText = `${productos.prenda}`
         clon.querySelector('.card-text').innerText = `${'$' + productos.precio}`
-        clon.querySelector('.talle').innerText = `${'Talle: ' + productos.talle}`
+        clon.querySelector('.talleUno').innerText = `${'Talle: ' + productos.talleUno + ' , ' + productos.talleDos + ' , ' + productos.talleTres}`
         clon.querySelector('.color').innerText = `${'Color: ' + productos.color}`
         clon.querySelector('.imagen').src = `${productos.imagen}`
         clon.querySelector('.filtros').innerText = `${productos.tipo}`
-        
+
+        let contadorProductos = document.createElement('h3')
+
+        contadorProductos.textContent = `${productos.stock}`
+
+        let li = document.createElement('p');
+
+        li.textContent = `${productos.prenda}`;
+
+        let p = document.createElement('p');
+
+        p.textContent = `${'$' + productos.precio}`;
+
+
+
+        let botonCarrito = document.createElement('button')
+
+        botonCarrito.innerHTML = `❌`
+
+        botonCarrito.addEventListener('click', () => {
+
+            carritoVacio = 0
+
+            li.remove();
+            p.remove();
+            botonCarrito.remove();
+            contadorProductos.remove();
+
+        })
+
+        let contador = 0
+
         //~~~~~~~~~~~~~~  evento agregar producto carrito ~~~~~~~~~~~~~~
 
         clon.querySelector('.delete').addEventListener('click', () => {
 
-
-            let li = document.createElement('p');
-
-            li.textContent = `${productos.prenda}`;
+            carritoDeCompras.appendChild(contadorProductos);
 
             carritoDeCompras.appendChild(li);
 
-            let p = document.createElement('p');
-
-            p.textContent = `${'$' + productos.precio}`;
-
             carritoDeCompras.appendChild(p);
-
-            let botonCarrito = document.createElement('button');
-
-            botonCarrito.textContent = `Comprar`
 
             carritoDeCompras.appendChild(botonCarrito);
 
+
+
+            contador += 1;
+            contadorProductos.innerText = contador
+
+
             //~~~~~~~~~~~~~ eventos para vaciar carrito ~~~~~~~~~~~~
+
 
             carritoVacio = 1;
 
@@ -109,7 +137,7 @@ function render_ropa(data) {
                 li.remove();
                 p.remove();
                 botonCarrito.remove();
-
+                contadorProductos.remove();
             })
 
             vaciarCarrito.querySelector('.botonComprar').addEventListener('click', () => {
@@ -128,25 +156,42 @@ function render_ropa(data) {
                 li.remove();
                 p.remove();
                 botonCarrito.remove();
+                contadorProductos.remove();
             })
 
 
+            const inputOptions = new Promise((resolve) => {
+                setTimeout(() => {
+                  resolve({
+                    "talleUno": `${productos.talleUno}`,
+                    "talleDos": `${productos.talleDos}`,
+                    "talleTres": `${productos.talleTres}`,
 
-            Toastify({
-                text: `item agregado al carrito`,
-                duration: 2000,
-                destination: "https://github.com/apvarun/toastify-js",
-                newWindow: true,
-                close: true,
-                gravity: "bottom",
-                position: "left",
-                stopOnFocus: true,
-                style: {
-                    background: "linear-gradient(to right, #0a0908, #0a0908)",
-                },
-                onClick: function () { } // Callback after click
-            }).showToast();
-
+                  });
+                }, 1000);
+              });
+              const { value: color } =  Swal.fire({
+                title: "Selecciona un talle",
+                input: "radio",
+                inputOptions,
+              }).then((result) => {
+                if (result.isConfirmed) {
+                    Toastify({
+                        text: `item agregado al carrito`,
+                        duration: 2000,
+                        newWindow: true,
+                        close: true,
+                        gravity: "bottom",
+                        position: "left",
+                        stopOnFocus: true,
+                        style: {
+                            background: "linear-gradient(to right, #0a0908, #0a0908)",
+                        },
+                        onClick: function () { } // Callback after click
+                    }).showToast();
+                    
+                }
+            })
         })
 
         productos_disponibles.appendChild(clon)
